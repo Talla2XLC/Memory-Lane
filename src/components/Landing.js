2c9689688d76f113e9c9373e2312943dfe089ae5
Alex V <al.voytovsky@gmail.com>
2020-04-17 02:39:59 +0300

    LandingForm as JSX imported

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,7 +4,7 @@ import LandingHeader from './Landing/LandingHeader.js';
 import LandingAbout from './Landing/LandingAbout.js';
 import LandingMission from './Landing/LandingMission.js';
 import LandingFeatures from './Landing/LandingFeatures';
-import LandingForm from './Landing/LandingForm.js';
+import LandingForm from './Landing/LandingForm';
 
 export default class Landing extends Component {  
   render() {
