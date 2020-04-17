3f716df6f0d6d6693bf6a36e83c8c203db28dd6d
Alex V <al.voytovsky@gmail.com>
2020-04-17 13:49:13 +0300

    line 1: { Component } removed from import

--- src/components/Landing/LandingAbout.js
+++ src/components/Landing/LandingAbout.js
@@ -1,4 +1,4 @@
-import React, { Component }             from 'react';
+import React                            from 'react';
 import styled                           from 'styled-components';
 import { ReactComponent as FamilySVG }  from './svg/about_picture.svg';
 
