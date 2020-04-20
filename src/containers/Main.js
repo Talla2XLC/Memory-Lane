ba38a883181da8be0e9be4040d41c40bc0d9cec8
Alex V <al.voytovsky@gmail.com>
2020-04-20 20:53:15 +0300

    state added for a while

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,15 +1,27 @@
-import React, { Component } from 'react';
-import './Main.sass';
-import Header from "../components/Main/Header";
-import MainNav from "../components/Main/MainNav";
+import React, { Component } from 'react'
+import './Main.sass'
+import Header from '../components/Main/Header';
+import MainNav from '../components/Main/MainNav';
 
-export default class Landing extends Component {
+export default class Main extends Component {
+  state = {
+    navItems: [
+      { endpoint: 'persons', title: 'Персоны' },
+      { endpoint: 'albums', title: 'Альбомы' },
+      { endpoint: 'stories', title: 'Истории' },
+      { endpoint: 'services', title: 'Сервисы' },
+      { endpoint: 'learn', title: 'Обучение' },
+    ]
+  }
+
   render() {
+    const { navItems } = this.state
+
     return (
       <div className="Main">
         <Header />
-        <MainNav />
+        <MainNav navItems={ navItems } />
       </div>
-    );
+    )
   }
 }
