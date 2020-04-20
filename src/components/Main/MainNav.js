e0a4d7778713c6b4f71eaf4dfd69fab132f75fd1
Alex V <al.voytovsky@gmail.com>
2020-04-20 21:47:25 +0300

    MainNav test

--- src/components/Main/MainNav.js
+++ src/components/Main/MainNav.js
@@ -1,9 +1,36 @@
-import React from 'react';
-import './MainNav.sass';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import shortid from 'shortid'
+import './MainNav.sass'
 
-export default function MainNav() {
-  return (
-    <div className="MainNav">
-    </div>
-  );
+export default class MainNav extends Component {
+  static propTypes = {
+    navItems: PropTypes.object.isRequired
+  }
+
+  render() {
+    const { navItems } = this.props
+      
+    const navigationItems = navItems.map(navItem =>
+      (
+        <div className="navigation-item">
+          <img src="" alt="">
+          </img>
+
+          <Link key={ shortid.generate() } to={ `/${navItem.endpoint}/` }>
+            {navItem.title}
+          </Link>
+        </div>
+      ) 
+    )
+
+    return (
+      <div className="MainNav">
+        { navigationItems }
+      </div>
+    )
+  }
 }
+
+
