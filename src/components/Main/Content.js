2bdbc9577b21f7e6dd7c64e096ff7a151b065b2e
Talla2XLC <mmm-appp@mail.ru>
2020-04-22 15:26:20 +0300

    finalized ESlint warnings

--- src/components/Main/Content.js
+++ src/components/Main/Content.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Content.sass';
 import Router from "../../Router";
-import {Provider} from "react-redux";
+// import {Provider} from "react-redux";
 
 export default function Content() {
   return (
