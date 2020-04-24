85b471a3e660b4a3c28b5ebf6e14d4aa76928948
bogdanovaO <olya@MacBook-Pro-Olya.local>
2020-04-24 16:44:48 +0300

    fix styles

--- src/components/Main/Albums/AlbumsItem.jsx
+++ src/components/Main/Albums/AlbumsItem.jsx
@@ -6,18 +6,16 @@ import { ReactComponent as Forward } from './svg/forward.svg';
 export default class AlbumsItem extends Component {
   render() {
     return (
-      <div className='albumsWrapper'>
+      <div>
         <img className='img' src={this.props.url} alt='galery_pic'/>
-        <div className='info'>
           <span className='itemName'>{this.props.name}</span>
           <div className='icons'>
-            <Download className='download' />
+            <Download className='downloadIcon' />
             <Forward/>
           </div>
           <div className='itemAutor'>{this.props.autor}</div>
           <div className='itemDate'>{this.props.date}</div>
           <div className='itemDescription'>Ipsum ex commodo ad ad ipsum non quis laborum adipisicing reprehenderit aliqua veniam excepteur. Sint dolor Lorem cupidatat et adipisicing anim adipisicing velit. Ullamco mollit occaecat nisi amet pariatur ad labore reprehenderit est aute.</div>
-        </div>
       </div>
     );
   }
