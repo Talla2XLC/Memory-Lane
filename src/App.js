04a38b1e2d0397097452f88872754a630563a309
GeekBrains-Legacy <63612027+GeekBrains-Legacy@users.noreply.github.com>
2020-05-01 01:31:26 +0300

    Change data display

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import './App.sass';
 import Landing from './containers/Landing.js';
 import Main from './containers/Main.js';
-import AuthorizationForm from './components/Main/AuthorizationForm';
-import RegistrationForm from './components/Main/RegistrationForm';
-import RegistrationFormName from './components/Main/RegistrationFormName';
 
 class App extends Component {
   constructor(props) {
@@ -15,11 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* {this.state.showLanding ? <Landing/> : <Main/>} */}
-        <RegistrationForm /> 
-        <AuthorizationForm />
-        <RegistrationFormName />
-        {/* <Landing /> */}
+        {this.state.showLanding ? <Landing/> : <Main/>}
       </div>
     );
   }
