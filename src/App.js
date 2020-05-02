c300f84c5b902dd38b92fee3bf24fbf576d14d46
GeekBrains-Legacy <63612027+GeekBrains-Legacy@users.noreply.github.com>
2020-05-02 17:28:03 +0300

    fix style

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import './App.sass';
 import Landing from './containers/Landing.js';
 import Main from './containers/Main.js';
 
-import RegistrationForm from './components/Main/RegistrationForm';
-import RegistrationFormName from './components/Main/RegistrationFormName';
-import AuthorizationForm from './components/Main/AuthorizationForm';
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -16,11 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* {this.state.showLanding ? <Landing/> : <Main/>} */}
-        {/* <Landing /> */}
-        <RegistrationForm />
-        <RegistrationFormName />
-        <AuthorizationForm />
+        {this.state.showLanding ? <Landing/> : <Main/>}
       </div>
     );
   }
