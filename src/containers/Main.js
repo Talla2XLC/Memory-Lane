260eba2334842633abc5dd6facacea4c7379b3f4
Alex V <al.voytovsky@gmail.com>
2020-05-15 23:07:33 +0300

    skipping introduce logic implementation completed (redux included)

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -36,7 +36,7 @@ class Main extends Component {
 
   render() {
     const { navItems } = this.state;
-    const { loading, isAuthorized, headerHeight, hasFullName } = this.props;
+    const { loading, isAuthorized, headerHeight, hasFullName, askedToIntroduce } = this.props;
 
     return (
       <BrowserRouter>
@@ -44,7 +44,7 @@ class Main extends Component {
           isAuthorized ?
             (loading ?
               <h1>Загрузка данных</h1> :
-              hasFullName ?
+              (hasFullName || askedToIntroduce) ?
                 (<MainWrapper className='Main' headerHeight={headerHeight}>
                   <Header headerHeight={this.setHeaderHeight}/>
                   <PerfectScrollbar component='div'>
@@ -55,7 +55,7 @@ class Main extends Component {
                   </PerfectScrollbar>
                 </MainWrapper>) :
 
-                <Router isAuthorized={isAuthorized} hasFullName={hasFullName}/>
+                <Router isAuthorized={isAuthorized} hasFullName={hasFullName} />
             )
 
             : <Router isAuthorized={isAuthorized}/>
@@ -90,7 +90,8 @@ const mapStateToProps = state => {
     error: state.userInfo.error,
     isAuthorized: state.session.isAuthorized,
     currentUser: state.userInfo.currentUser,
-    hasFullName: !!(state.userInfo.currentUser.first_name || state.userInfo.currentUser.last_name)
+    hasFullName: !!(state.userInfo.currentUser.first_name || state.userInfo.currentUser.last_name),
+    askedToIntroduce: state.userInfo.currentUser.asked_to_introduce
   };
 };
 
