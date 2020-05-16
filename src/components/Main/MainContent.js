aa99027d7cc5dc93bd6cef532725e2f5a82c5d8e
UncleScrooge2012 <naruk2024@gmail.com>
2020-05-17 00:21:05 +0300

    MainContent
    add Header

--- src/components/Main/MainContent.js
+++ src/components/Main/MainContent.js
@@ -1,47 +1,64 @@
 import React from 'react';
 import './MainContent.sass';
 import NewsFeed from './NewsFeed';
-import {ButtonContainer} from './Button.jsx';
+import { ButtonContainer } from './Button.jsx';
+import { ReactComponent as HeaderImg } from './svg/Image.svg';
 
 export default function MainContent() {
-  return  <div className='contentContainer'>
+  return <div className='contentContainer'>
+
+    <div className='MainContent__header'>
+      <div className='MainContent__header-inner'>
+
+        <div className='MainContent__header-info'>
+          <h2 className='MainContent__header-titel'>
+            Здравствуйте!
+
+            </h2>
+
+          <p className='MainContent__header-text'>
+            Давайте вместе создадим
+            вашу историю, чтобы остаться
+            в памяти у потомков!
+            </p>
+          
+          <ButtonContainer >Перейти</ButtonContainer>
+
+
+
+
+        </div>
+
+        <div className='MainContent__header-imj'>
+          <HeaderImg />
+
+        </div>
+
+      </div>
+
+    </div>
     <div className='mainContainer'>
 
-      <div className='mainContainer__item div1 white basicItem'>
-        <div className='mainContainer__item_left'>
-          <p className='head1 white' >Здравствуйте!</p>
-          <p className='mainText textPosition'>
-            Давайте вместе создадим<br/>
-            вашу историю, чтобы остаться<br/>
-            в памяти у потомков!
-          </p>
-        </div>
-        <div className='mainContainer__item_right'>
-          <p className='mainText textPosition'>
-            Чтобы увеличить<br/> лимит фотографий,<br/> измените тариф
-          </p>
-          <ButtonContainer white={true}>Перейти</ButtonContainer>
-        </div>
-      </div>
+
 
       <div className='mainContainer__item div2 basicItem' >
         <p className='head2'>Не знаете с чего начать?</p>
         <p className='mainText textPosition'>
-            Наши задания помогут вам наполнить историю воспоминаниями
+          Наши задания помогут вам наполнить историю воспоминаниями
         </p>
         <ButtonContainer>Перейти</ButtonContainer>
       </div>
-      
+
       <div className='mainContainer__item div3 basicItem'>
         <p className='head2'>Пригласить друга</p>
         <p className='mainText textPosition'>
-            Пригласите своего близкого человека,<br/> чтобы он стал частью вашей истории.
+          Пригласите своего близкого человека,<br /> чтобы он стал частью вашей истории.
         </p>
         <ButtonContainer> Пригласить</ButtonContainer>
       </div>
 
       <div className='mainContainer__item div4 basicItem' />
     </div>
-    <NewsFeed/>
+    <NewsFeed />
   </div>;
 }
