6d1dfce55a3f52b7214193d9981daf76dd8b224b
Alex V <al.voytovsky@gmail.com>
2020-05-16 17:51:35 +0300

    stories reducer: get stories

---
+++ src/reducers/stories.js
@@ -0,0 +1,30 @@
+const initialState = {
+  stories: null
+};
+
+export default function storiesInfo(state = initialState, action) {
+  switch (action.type) {
+    case 'GET_STORIES_START':
+      return {
+        ...state,
+        loading: true
+      };
+    case 'GET_STORIES_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        isAuthorized: true,   // Not sure about this
+        stories: action.payload,
+        error: null
+      };
+    case 'GET_STORIES_FAILED':
+      return {
+        ...state,
+        loading: false,
+        isAuthorized: false,   // Not sure about this
+        error: action.payload.err
+      };
+    default:
+      return state;
+  }
+}
