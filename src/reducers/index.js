8aca555709b8dc4764a3727655526e15020ce451
Alex V <al.voytovsky@gmail.com>
2020-05-16 17:54:37 +0300

    userStories added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import userInfo from './user';
 import session from './session';
+import userStories from './stories';
 
 const rootReducer = combineReducers({
   userInfo,
-  session
+  session,
+  userStories
 })
 
 export default rootReducer;
