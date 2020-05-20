c2a80abd74813cf3078c6c59d022e130c867e099
Alex V <al.voytovsky@gmail.com>
2020-05-20 19:56:28 +0300

    EmptyBlock removed if there are no stories

--- src/components/Main/Stories/AddStory.jsx
+++ src/components/Main/Stories/AddStory.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PerfectScrollbar from 'react-perfect-scrollbar';
-import EmptyBlock from '../EmptyBlock/EmptyBlock';
 
 import { fetchStories } from '../../../actions/actionStories';
 
@@ -24,30 +23,26 @@ class AddStory extends Component {
       loading ?
         <h1>Загрузка данных</h1> :
         (<PerfectScrollbar>
-          {
-            Object.keys(stories).length === 0 ?
-              <div className='contentContainer'> <EmptyBlock/> </div> :
-              (
-                <div className='addStory'>
-                  <div className='addStory__desk'>
-                    <div className='font1Light'>Добавьте заголовок к вашей истории</div>
-                    <div>тег</div>
-                    <div className='font1Light'>{this.props.autor}</div>
-                    <div className='font1Light'>{this.props.date}</div>
-                    <div>country</div>
-                  </div>
-                  <div className='addStory__photo'>
-                    <img className='addPhotoItem' src='https://picsum.photos/168/168' />
-                    <img className='addPhotoItem' src='https://picsum.photos/168/168' />
-                    <img className='addPhotoItem' src='https://picsum.photos/168/168' />
-                    <img className='addPhotoItem' src='https://picsum.photos/168/168' />
-                    <img className='addPhotoItem' src='https://picsum.photos/168/168' />
+          {    
+            <div className='addStory'>
+              <div className='addStory__desk'>
+                <div className='font1Light'>Добавьте заголовок к вашей истории</div>
+                <div>тег</div>
+                <div className='font1Light'>{this.props.autor}</div>
+                <div className='font1Light'>{this.props.date}</div>
+                <div>country</div>
+              </div>
+              <div className='addStory__photo'>
+                <img className='addPhotoItem' src='https://picsum.photos/168/168' />
+                <img className='addPhotoItem' src='https://picsum.photos/168/168' />
+                <img className='addPhotoItem' src='https://picsum.photos/168/168' />
+                <img className='addPhotoItem' src='https://picsum.photos/168/168' />
+                <img className='addPhotoItem' src='https://picsum.photos/168/168' />
 
-                  </div>
-                  <textarea className='addStory__textArea' />
-                  <button>Опубликовать</button>
-                </div>
-              )
+              </div>
+              <textarea className='addStory__textArea' />
+              <button>Опубликовать</button>
+            </div>  
           }
         </PerfectScrollbar>)
     );
