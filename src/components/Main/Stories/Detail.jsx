2291946ca3f02685a55f271b602f60fc0057758c
Alex V <al.voytovsky@gmail.com>
2020-05-22 00:18:11 +0300

    class component instead of functional one

--- src/components/Main/Stories/Detail.jsx
+++ src/components/Main/Stories/Detail.jsx
@@ -1,34 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import './Stories.sass';
 // import { detailStory } from './storiesData';
 
-function Detail() {
-  return (
-    <PerfectScrollbar component='div'>
-      <div className='contentContainer details' >
-        <img className='details__photo' src="" alt='detailsPhoto'/>
-        <div className='details__right'>
-          <div className='main1'>Заголовок</div>
-          <div>тег</div>
-          <div className='text3'></div>
-          <div className='text3'></div>
-          <div>country</div>
-          <div className='text1'> Eiusmod consectetur ullamco nostrud nisi laborum ullamco nulla non est velit. </div>
-          <div className='action'>
-            <div>Редактировать</div>
+export default class Detail extends Component {
+
+  // /db/updateHistory
+
+  render() {
+
+    return (
+      <PerfectScrollbar component='div'>
+        <div className='contentContainer details'>
+          <img className='details__photo' src='' alt='detailsPhoto'/>
+          <div className='details__right'>
+            <div className='main1'>Заголовок</div>
+            <div>тег</div>
+            <div className='text3'></div>
+            <div className='text3'></div>
+            <div>country</div>
+            <div className='text1'> Eiusmod consectetur ullamco nostrud nisi laborum ullamco nulla non est velit. </div>
+            <div className='action'>
+              <button>Редактировать</button>
+            </div>
+          </div>
+          <div className='details__left'>
+            <div className='details__title main1'>Заголовок</div>
+            <div />
+            <div className='desk text1'> Eiusmod consectetur ullamco nostrud nisi laborum ullamco nulla non est velit.Eiusmod consectetur ullamco nostrud nisi laborum ullamco nulla non est velit.Eiusmod consectetur ullamco nostrud nisi laborum ullamco nulla non est velit.</div>
           </div>
         </div>
-        <div className='details__left'>
-          <div className='details__title main1'>Заголовок</div>
-          <div />
-          <div className='desk text1'> Eiusmod consectetur ullamco nostrud nisi laborum ullamco nulla non est velit.Eiusmod consectetur ullamco nostrud nisi laborum ullamco nulla non est velit.Eiusmod consectetur ullamco nostrud nisi laborum ullamco nulla non est velit.</div>
-        </div>
-      </div>
-    </PerfectScrollbar>
-  );
+      </PerfectScrollbar>
+    );
+  };
 }
-
-export default Detail;
-
-
