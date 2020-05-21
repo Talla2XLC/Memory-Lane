d8390d4e296d99a2150ba36abab2d13d8024f18c
Alex V <al.voytovsky@gmail.com>
2020-05-21 23:35:53 +0300

    button towards the Add Story page logic now

--- src/components/Main/EmptyBlock/EmptyBlock.jsx
+++ src/components/Main/EmptyBlock/EmptyBlock.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
-import './EmptyBlock.sass';
-import {ReactComponent as FamilySvg} from '../svg/family.svg';
-import {ButtonContainer} from '../Button';
+import { Link, withRouter } from 'react-router-dom';
 
-export default class EmptyBlock extends Component {
+import './EmptyBlock.sass';
+
+import { ReactComponent as FamilySvg } from '../svg/family.svg';
+import { ButtonContainer } from '../Button';
+
+class EmptyBlock extends Component {
+
+  forwardToAddStory = () => this.props.history.push('/stories/add');
+
   render() {
     return (
       <div className='emptyBlock'>
         <FamilySvg className='familySvg' />
         <div className='head2'>Здесь пока нет ни одной записи</div>
-        <ButtonContainer className='emptyNewsBlockButton' >Перейти</ButtonContainer>
+        <ButtonContainer
+          className='emptyNewsBlockButton'
+          onClick={this.forwardToAddStory}
+        >
+          Перейти
+        </ButtonContainer>
       </div>
     );
-  }
+  };
 }
+
+export default withRouter(EmptyBlock);
