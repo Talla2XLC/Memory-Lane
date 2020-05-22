df11607169213a179bd8f6a059cf68ff835efcbf
GeekBrains-Legacy <63612027+GeekBrains-Legacy@users.noreply.github.com>
2020-05-22 16:32:58 +0300

    Creating Page Background Not Found 404

---
+++ src/components/Main/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './UserFormStyle.css';
+
+export default class PageNotFound extends Component {
+    render () {
+        return (
+            <div className='container'>
+                
+            </div>
+        )
+    }
+}
