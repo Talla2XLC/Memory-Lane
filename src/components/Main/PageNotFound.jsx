0570d1d57598127be3e6d10d1171dea4664f0358
GeekBrains-Legacy <63612027+GeekBrains-Legacy@users.noreply.github.com>
2020-05-22 16:51:28 +0300

    Button added

--- src/components/Main/PageNotFound.jsx
+++ src/components/Main/PageNotFound.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './UserFormStyle.css';
+import { ButtonContainer } from './Button.jsx';
 
 export default class PageNotFound extends Component {
     render () {
         return (
             <div className='container'>
-                
+                <ButtonContainer className='container__btn'>Вернуться на главную</ButtonContainer>
             </div>
         )
     }
