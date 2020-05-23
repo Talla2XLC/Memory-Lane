754af9e940419cc5e919106d963c971d1d2b46cf
Alex V <al.voytovsky@gmail.com>
2020-05-23 13:07:58 +0300

    initial code reverting

--- src/components/Main/EmptyBlock/EmptyBlock.jsx
+++ src/components/Main/EmptyBlock/EmptyBlock.jsx
@@ -1,29 +1,20 @@
 import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
 
 import './EmptyBlock.sass';
 
 import { ReactComponent as FamilySvg } from '../svg/family.svg';
 import { ButtonContainer } from '../Button';
 
-class EmptyBlock extends Component {
-
-  forwardToAddStory = () => this.props.history.push('/stories/add');
-
+export default class EmptyBlock extends Component {
   render() {
     return (
       <div className='emptyBlock'>
         <FamilySvg className='familySvg' />
         <div className='head2'>Здесь пока нет ни одной записи</div>
-        <ButtonContainer
-          className='emptyNewsBlockButton'
-          onClick={this.forwardToAddStory}
-        >
+        <ButtonContainer className='emptyNewsBlockButton'>
           Перейти
         </ButtonContainer>
       </div>
     );
   };
 }
-
-export default withRouter(EmptyBlock);
