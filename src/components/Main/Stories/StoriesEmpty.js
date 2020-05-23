40775b6845098e6a204cbf0538e2e5c941cceb3f
Alex V <al.voytovsky@gmail.com>
2020-05-23 13:13:33 +0300

    renamed and .jsx to .js changed

---
+++ src/components/Main/Stories/StoriesEmpty.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+import './EmptyBlock.sass';
+
+import { ReactComponent as FamilySvg } from '../svg/family.svg';
+import { ButtonContainer } from '../Button';
+
+class EmptyBlock extends Component {
+
+  // this.props.match.params
+
+  forwardToAddStory = () => this.props.history.push('/stories/add');
+
+  render() {
+    return (
+      <div className='emptyBlock'>
+        <FamilySvg className='familySvg' />
+        <div className='head2'>Здесь пока нет ни одной записи</div>
+        <ButtonContainer
+          className='emptyNewsBlockButton'
+          onClick={this.forwardToAddStory}
+        >
+          Перейти
+        </ButtonContainer>
+      </div>
+    );
+  };
+}
+
+export default withRouter(EmptyBlock);
