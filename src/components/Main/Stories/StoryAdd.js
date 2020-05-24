29b6d73ba55403aa1eddb94b2c0f2d45d7d629ea
Alex V <al.voytovsky@gmail.com>
2020-05-24 17:29:11 +0300

    experimental button removed

--- src/components/Main/Stories/StoryAdd.js
+++ src/components/Main/Stories/StoryAdd.js
@@ -135,7 +135,7 @@ export default class AddStory extends Component {
               />
 
               <div className='storyAdd__wrapper'>
-                <ButtonEncircledCross />
+                // <ButtonEncircledCross />
                 <textarea
                   className='text1 storyAdd__content'
                   name='content'
