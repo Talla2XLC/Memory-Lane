96e028e2ec21934c8448ce2fcf4d8e3fe3654383
Alex V <al.voytovsky@gmail.com>
2020-05-24 03:04:27 +0300

    necessary changes due to the implementing current tasks

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import userInfo from './user';
 import session from './session';
+import userStories from './stories';
 
 import albums from './albums';
 const rootReducer = combineReducers({
   userInfo,
   session,
-  albums
+  // albums,
+  userStories
 });
 
 export default rootReducer;
